871b5720109292be81e689784796b6896047888b
sudiptapaul <psudipta858@gmail.com>
2025-03-09 11:41:54 +0530

    mongodb setup

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private configService: ConfigService,
+    @InjectConnection() private readonly connection: Connection
+  ) {}
 
   getDatabaseUrl(): string {
     return this.configService.get<string>('DATABASE_URL') ?? '';
+  }
+
+  async checkDatabaseConnection(): Promise<string> {
+    return this.connection.readyState === 1 ? 'Connected' : 'Disconnected';
   }
 }
