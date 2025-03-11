b949e8c7bc540f401205f464b1abf03dc4b1bd41
JudeTulel <judeyego202@gmail.com>
2025-03-11 04:08:42 +0300

    conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { validate } from './config/validation/env.validation';
+import { validate } from './config/env.validation';
 
 @Module({
   imports: [
