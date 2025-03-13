544a9fc92052d95cdef884bf5fd0b225ae841961
Usman Adamu <imkargi@gmail.com>
2025-03-13 01:24:36 +0000

    Updated the songModule with the required imports and config

--- src/song/song.module.ts
+++ src/song/song.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { SongController } from './song.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { SongService } from './song.service';
+import { SongController } from './song.controller';
+import { Song, SongSchema } from 'src/schemas/song.chema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Song.name, schema: SongSchema }])],
   controllers: [SongController],
-  providers: [SongService]
+  providers: [SongService],
+  exports: [SongService]
 })
 export class SongModule {}
